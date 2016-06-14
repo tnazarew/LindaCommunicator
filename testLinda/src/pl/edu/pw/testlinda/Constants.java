b33bbf186f9031498f8e3d2dde990845d25ca1db
m2xx <P.Malkiewicz@stud.elka.pw.edu.pl>
2016-06-14 20:57:00 +0200

    Cleaning tmp files works now

--- testLinda/src/pl/edu/pw/testlinda/Constants.java
+++ testLinda/src/pl/edu/pw/testlinda/Constants.java
@@ -2,5 +2,6 @@ package pl.edu.pw.testlinda;
 
 public class Constants {
     public static String path;
-    public static String default_prefix;
-}
+    public static String msgFilePrefix;
+    public static String procFilePath;
+    public static String tupleFilePath;}
--- testLinda/src/pl/edu/pw/testlinda/Main.java
+++ testLinda/src/pl/edu/pw/testlinda/Main.java
@@ -7,15 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         //path to linda
-        Constants.path = "/home/wojtek/projekty/LindaCommunicator/main";
-        Constants.default_prefix = "/tmp/lin_mes";
+        Constants.path = "/home/juzer/.CLion2016.1/system/cmake/generated/LindaCommunicator-103156cf/103156cf/Debug/main";
+        Constants.msgFilePrefix = "/tmp/lin_mes";
+        Constants.procFilePath = "/tmp/proc_test";
+        Constants.tupleFilePath = "/tmp/tuple_test";
 
         ArrayList<Test> tests = new ArrayList<>();
         tests.add(new Test1(10));
-//        tests.add(new Test2(10));
-//        tests.add(new Test3(10, 10));
-//        tests.add(new Test4(10));
-//        tests.add(new Test5(10));
+        tests.add(new Test2(10));
+        tests.add(new Test3(10, 10));
+        tests.add(new Test4(10));
+        tests.add(new Test5(10));
 
         for (Test test : tests) {
             System.out.println("----------------------------------------------------");
