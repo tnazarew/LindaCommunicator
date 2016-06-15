efe36264f616b087e1cb4efc7f92de0c1bdc257e
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2016-06-15 02:25:14 +0200

    Code clearance

--- src/LindaCommunicator.cpp
+++ src/LindaCommunicator.cpp
@@ -1,7 +1,3 @@
-//
-// Created by tomasz on 07.06.16.
-//
-
 #include <LindaCommunicator.h>
 
 #include <algorithm>
@@ -11,13 +7,13 @@
 
 #include <signal.h>
 #include <linda_exception.h>
-
+//*********************************************************************************************************************
 void sigusr1Handler(int, siginfo_t*, void*) { }
 
 linda::LindaCommunicator::LindaCommunicator(std::string tuple_file, std::string process_file)
 {
-    tuple_fd = open((DEFAULT_FILEPATH+tuple_file).c_str(), O_RDWR|O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-    proc_fd = open((DEFAULT_FILEPATH+process_file).c_str(), O_RDWR|O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    tuple_fd = open((DEFAULT_FILEPATH+tuple_file).c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    proc_fd = open((DEFAULT_FILEPATH+process_file).c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 
 
     // Set SIGUSR1 handler
@@ -40,31 +36,31 @@ linda::LindaCommunicator::LindaCommunicator(std::string tuple_file, std::string 
         throw linda::LindaException("Failed to block SIGUSR1");
 
 }
-
+//*********************************************************************************************************************
 linda::LindaCommunicator::~LindaCommunicator()
 {
     close(tuple_fd);
     close(proc_fd);
 }
-
+//*********************************************************************************************************************
 void linda::LindaCommunicator::sortQueue(std::vector<ProcessFileUtils::process> &queue)
 {
     std::sort(queue.begin(), queue.end());
-
 }
-
+//*********************************************************************************************************************
 void linda::LindaCommunicator::wakeProcesses(linda::TupleFileUtils::tuple *tuple)
 {
     std::vector<linda::ProcessFileUtils::process> pids = linda::MatchesFinder::returnProcessQueue(proc_fd, tuple);
     sortQueue(pids);
-    bool input = 0;
-    for (ProcessFileUtils::process pid: pids) {
+    int input = 0;
+    for (ProcessFileUtils::process pid : pids) {
         if(!input) {
             ProcessFileUtils::process ptr;
             ProcessFileUtils::readRecord(proc_fd, &ptr, pid.record_id);
             ptr.found = 1;
             ProcessFileUtils::writeRecord(proc_fd, &ptr, pid.record_id);
             ProcessFileUtils::unlockRecord(proc_fd, sizeof(ptr), ptr.record_id);
+
             int temp_fd = open((DEFAULT_FILEPATH + DEF_MES_FILE_PREF + std::to_string(pid.pid)).c_str(), O_RDWR | O_CREAT,
                                S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 
@@ -76,7 +72,8 @@ void linda::LindaCommunicator::wakeProcesses(linda::TupleFileUtils::tuple *tuple
             }
 
             ProcessFileUtils::wakeupProcess(pid.pid);
-            if (ptr.flag) {
+            if (ptr.flag)
+            {
                 input = 1;
             }
         }
@@ -87,17 +84,17 @@ void linda::LindaCommunicator::wakeProcesses(linda::TupleFileUtils::tuple *tuple
     }
     TupleFileUtils::unlockRecord(tuple_fd, sizeof(*tuple), tuple->record_id);
 }
-
+//*********************************************************************************************************************
 linda::TupleFileUtils::tuple linda::LindaCommunicator::input(std::string pattern)
 {
     return read_(pattern, true);
 }
-
+//*********************************************************************************************************************
 linda::TupleFileUtils::tuple linda::LindaCommunicator::read(std::string pattern)
 {
     return read_(pattern, false);
 }
-
+//*********************************************************************************************************************
 void linda::LindaCommunicator::output(std::string tuple)
 {
     int rec_id = TupleFileUtils::findAndLock(tuple_fd);
@@ -110,29 +107,32 @@ void linda::LindaCommunicator::output(std::string tuple)
     TupleFileUtils::writeRecord(tuple_fd, &t, rec_id);
 
     wakeProcesses(&t);
-
-
 }
-
+//*********************************************************************************************************************
 linda::TupleFileUtils::tuple linda::LindaCommunicator::read_(std::string pattern, bool input)
 {
-    int rec_id = ProcessFileUtils::findAndLock(proc_fd); //wolne miejsce w pliku z procesami zakładamy że blokure obszar
+    int rec_id = ProcessFileUtils::findAndLock(proc_fd); // wolne miejsce w pliku z procesami zakładamy że blokuje obszar
     linda::ProcessFileUtils::process proc;
-    proc.initProcess(pattern, input, rec_id); // towrzymy strukture
-    ProcessFileUtils::writeRecord(proc_fd, &proc, proc.record_id); //wpisuje w obszar
-    ProcessFileUtils::unlockRecord(proc_fd, sizeof(proc), proc.record_id); //zwlania
+    proc.initProcess(pattern, input, rec_id); // tworzymy strukture
+    ProcessFileUtils::writeRecord(proc_fd, &proc, proc.record_id); // wpisuje w obszar
+    ProcessFileUtils::unlockRecord(proc_fd, sizeof(proc), proc.record_id); // zwalnia
+
     TupleFileUtils::tuple t = MatchesFinder::returnBlockedTuple(tuple_fd, &proc); // znajduje krotke dla wybranego procesu
     ProcessFileUtils::lockRecord(proc_fd, sizeof(proc), proc.record_id); // blokuje obszar
+
     if (t.record_id != -1) // krotka znaleziona
     {
-        if (proc.found) //inny proces znalazł juz krotke dla naszego procesu
+        if (proc.found) // inny proces znalazł juz krotke dla naszego procesu
         {
-            proc.taken = 0;
-            ProcessFileUtils::writeRecord(proc_fd, &proc, proc.record_id);
+//            proc.taken = 0;
+//            ProcessFileUtils::writeRecord(proc_fd, &proc, proc.record_id);
+//            ProcessFileUtils::unlockRecord(proc_fd, sizeof(proc), proc.record_id);
+//            TupleFileUtils::unlockRecord(tuple_fd, sizeof(t), t.record_id);
+
             ProcessFileUtils::unlockRecord(proc_fd, sizeof(proc), proc.record_id);
-            TupleFileUtils::unlockRecord(tuple_fd, sizeof(t), t.record_id);
             linda::sigusr1Suspend();
         }
+
         else // nie znaleziono dla nas krotki, nasza jest tą jedyną
         {
             if (proc.flag) // input
@@ -147,17 +147,17 @@ linda::TupleFileUtils::tuple linda::LindaCommunicator::read_(std::string pattern
             return t;
         }
     }
+
     else
     {
         ProcessFileUtils::unlockRecord(proc_fd, sizeof(proc), proc.record_id);
 
         linda::sigusr1Suspend();
     }
-    int new_fd = open((DEFAULT_FILEPATH + DEF_MES_FILE_PREF + std::to_string(proc.pid)).c_str(),O_RDWR , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+
+    int new_fd = open((DEFAULT_FILEPATH + DEF_MES_FILE_PREF + std::to_string(proc.pid)).c_str(), O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if(new_fd == -1)
         throw linda::LindaException("Unable to open auxiliary file");
-
-    std::cout << "@" << std::endl;
 
     ProcessFileUtils::lockRecord(proc_fd, sizeof(proc), rec_id);
     ProcessFileUtils::setRecordTaken(proc_fd, rec_id, 0);
@@ -166,10 +166,9 @@ linda::TupleFileUtils::tuple linda::LindaCommunicator::read_(std::string pattern
     TupleFileUtils::tuple mes_t;
     TupleFileUtils::readRecord(new_fd, &mes_t, 0);
 
-
     return mes_t;
 }
-
+//*********************************************************************************************************************
 void linda::ProcessFileUtils::process::initProcess(const std::string &pattern_, bool input, int rec_id)
 {
     timestamp = get_current_time();
@@ -184,5 +183,5 @@ void linda::ProcessFileUtils::process::initProcess(const std::string &pattern_, 
     pattern[pattern_.size()] = '\0';
     std::cout << pattern << std::endl;
 }
-
+//*********************************************************************************************************************
 
